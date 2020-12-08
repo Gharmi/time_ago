a837b13ddc5600a2a75959ef9bcdbb60b092eeb5
Sagar Paudel <=>
2020-12-08 15:20:54 +0545

    Updates the main file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:time_difference/main.dart';
+import 'package:time_difference/time_difference.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
